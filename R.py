e0e21750fe6ae199e06d90c5bbabad5f27b3ef65
Anox071 <153474617+Anox071@users.noreply.github.com>
2024-01-13 13:15:10 +0530

    Update R.py

--- R.py
+++ R.py
@@ -905,7 +905,7 @@ def rcrack(uid,pwx,tl):
 
 			"login":"Log In"}
 
-			header_freefb ={'authority': 'p.facebook.com',
+			header_freefb ={'authority': 'x.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'cache-control': 'max-age=0',
